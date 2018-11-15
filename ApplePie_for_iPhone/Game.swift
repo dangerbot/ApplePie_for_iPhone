d4432b3e5089cc309f2939f97f7db5908ecf2784
Steve <dangerboy@macbook-pro-3.local>
2018-11-14 17:13:54 -0800

    Added some logistics to set up game.

---
+++ ApplePie_for_iPhone/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  ApplePie_for_iPhone
+//
+//  Created by Steve on 11/14/18.
+//  Copyright © 2018 SteveAndTheDogs. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+} // END Game
--- ApplePie_for_iPhone/ViewController.swift
+++ ApplePie_for_iPhone/ViewController.swift
@@ -11,7 +11,13 @@ import os.log
 
 
 class ViewController: UIViewController {
-
+    
+    var listOfWords = ["apple","orange","fart"]
+    let numOfIncorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
+    
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var gameWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -23,17 +29,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         os_log("ViewController. viewDidLoad()", log: OSLog.default, type: .info)
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
 
     
     @IBAction func letterButtonPressedAction(_ sender: UIButton) {
         os_log("ViewController. letterButtonPressedAction()", log: OSLog.default, type: .info)
+        sender.isEnabled = false
         print(sender.isEnabled)
-        print(sender.title(for: .normal)!)
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        print(letter)
     }
     
+    func newRound() {
+        os_log("ViewController. newRound()", log: OSLog.default, type: .info)
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: numOfIncorrectMovesAllowed)
+        print("Word: \(currentGame.word) :: movesRemaining: \(currentGame.incorrectMovesRemaining)")
+        updateUI()
+    } // END newRound()
     
+    func updateUI() {
+        os_log("ViewController. updateUI()", log: OSLog.default, type: .info)
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        print("Wins: \(totalWins), Losses: \(totalLosses), Image: Tree \(currentGame.incorrectMovesRemaining).pdf")
+    } // END updateUI()
 
 }
 
