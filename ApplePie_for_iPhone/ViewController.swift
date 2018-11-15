6186eb1c0a343b2d74375cf1775eca014978b40c
Steve <dangerboy@macbook-pro-3.local>
2018-11-14 16:50:35 -0800

    Added Action and Outlets for buttons, image and labels. Test OK.

--- ApplePie_for_iPhone/ViewController.swift
+++ ApplePie_for_iPhone/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import os.log
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var gameWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        os_log("ViewController. viewDidLoad()", log: OSLog.default, type: .info)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func letterButtonPressedAction(_ sender: UIButton) {
+        os_log("ViewController. letterButtonPressedAction()", log: OSLog.default, type: .info)
+        print(sender.isEnabled)
+        print(sender.title(for: .normal)!)
+    }
+    
+    
 
 }
 
